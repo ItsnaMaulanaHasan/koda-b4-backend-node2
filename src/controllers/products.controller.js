303c4611aac6b7b94a8e7461429a67d87ef9414c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-25 14:10:06 +0700

    feat: added documentation for product api

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,12 @@
 const productsModel = require("../models/products.model");
 
+/**
+ * GET /products
+ * @summary Get list all of products
+ * @tags products
+ * @param {string} search.query - search by name of product
+ * @return {object} 200 - success get list all of products
+ */
 function listProducts(req, res) {
   const { search = "" } = req.query;
   const listProducts = productsModel.getListProducts(search);
@@ -11,6 +18,14 @@ function listProducts(req, res) {
   });
 }
 
+/**
+ * GET /products/{id}
+ * @summary Get detail product by id
+ * @tags products
+ * @param {number} id.path - id product
+ * @return {object} 200 - success get detail of product
+ * @return {object} 404 - product not found
+ */
 function detailProduct(req, res) {
   const { id } = req.params;
   const product = productsModel.getProductsById(parseInt(id));
@@ -30,16 +45,32 @@ function detailProduct(req, res) {
   });
 }
 
+/**
+ * POST /products
+ * @summary Create product
+ * @tags products
+ * @param {string} name.form.required - This is the name of product - application/x-www-form-urlencoded
+ * @param {number} price.form.required - This is the price of product - application/x-www-form-urlencoded
+ * @return {object} 201 - product added successfully
+ */
 function addProduct(req, res) {
   const data = req.body;
   productsModel.addDataProduct(data);
 
-  res.json({
+  res.status(201).json({
     success: true,
-    message: "Success add data product",
+    message: "Product added successfully",
   });
 }
 
+/**
+ * PATCH /products
+ * @summary Update product
+ * @tags products
+ * @param {string} name.form - This is the name of product - application/x-www-form-urlencoded
+ * @param {number} price.form - This is the price of product - application/x-www-form-urlencoded
+ * @return {object} 200 - product updated successfully
+ */
 function updateProduct(req, res) {
   const { id } = req.params;
   const data = req.body;
@@ -51,6 +82,13 @@ function updateProduct(req, res) {
   });
 }
 
+/**
+ * DELETE /products/{id}
+ * @summary Delete product
+ * @tags products
+ * @param {number} id.path - id product to delete
+ * @return {object} 200 - product updated successfully
+ */
 function deleteProduct(req, res) {
   const { id } = req.params;
   productsModel.deleteProductById(parseInt(id));
