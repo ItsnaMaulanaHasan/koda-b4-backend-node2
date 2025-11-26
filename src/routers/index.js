cdada0da5881008417ce76a1f7bcb52ffac44b96
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-26 17:09:54 +0700

    feat: applying auth middleware on product router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,14 @@
 import express, { Router } from "express";
 import { join } from "node:path";
 import { cwd } from "node:process";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import authRouter from "./auth.router.js";
 import productsRouter from "./products.router.js";
 
 const router = Router();
 
 router.use("/", authRouter);
-router.use("/", productsRouter);
+router.use("/", authMiddleware, productsRouter);
 router.use("/uploads", express.static(join(cwd(), "uploads")));
 
 export default router;
