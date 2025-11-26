f92395c64ab76b17f2a9cea00930b8b1bb68f909
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-26 11:52:01 +0700

    feat: adjust function to model for support prisma orm

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,149 +1,206 @@
-let products = [
-  {
-    id: 1,
-    image: null,
-    name: "Mixue Ice Cream",
-    price: 8000,
-  },
-  {
-    id: 2,
-    image: null,
-    name: "BOBA Sundae",
-    price: 16000,
-  },
-  {
-    id: 3,
-    image: null,
-    name: "Strawberry Mi-Shake",
-    price: 16000,
-  },
-  {
-    id: 4,
-    image: null,
-    name: "BOBA Mi-Shake",
-    price: 16000,
-  },
-  {
-    id: 5,
-    image: null,
-    name: "Chocolate Cookies Smoothies",
-    price: 16000,
-  },
-  {
-    id: 6,
-    image: null,
-    name: "Brown Sugar Pearl Milk Tea",
-    price: 19000,
-  },
-  {
-    id: 7,
-    image: null,
-    name: "Pearl Milk Tea",
-    price: 22000,
-  },
-  {
-    id: 8,
-    image: null,
-    name: "Oats Milk Tea",
-    price: 22000,
-  },
-  {
-    id: 9,
-    image: null,
-    name: "Coconut Jelly Milk Tea",
-    price: 22000,
-  },
-  {
-    id: 10,
-    image: null,
-    name: "Red Bean Milk Tea",
-    price: 22000,
-  },
-  {
-    id: 11,
-    image: null,
-    name: "Fresh Squeezed Lemonade",
-    price: 10000,
-  },
-  {
-    id: 12,
-    image: null,
-    name: "Peach Earl Grey Tea",
-    price: 16000,
-  },
-  {
-    id: 13,
-    image: null,
-    name: "Original Jasmine Tea",
-    price: 10000,
-  },
-  {
-    id: 14,
-    image: null,
-    name: "Original Earl Grey Tea",
-    price: 10000,
-  },
-];
+import { prisma } from "../lib/prisma";
 
-export function getTotalDataProducts(search) {
-  const results = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
-  );
+// let products = [
+//   {
+//     id: 1,
+//     image: null,
+//     name: "Mixue Ice Cream",
+//     price: 8000,
+//   },
+//   {
+//     id: 2,
+//     image: null,
+//     name: "BOBA Sundae",
+//     price: 16000,
+//   },
+//   {
+//     id: 3,
+//     image: null,
+//     name: "Strawberry Mi-Shake",
+//     price: 16000,
+//   },
+//   {
+//     id: 4,
+//     image: null,
+//     name: "BOBA Mi-Shake",
+//     price: 16000,
+//   },
+//   {
+//     id: 5,
+//     image: null,
+//     name: "Chocolate Cookies Smoothies",
+//     price: 16000,
+//   },
+//   {
+//     id: 6,
+//     image: null,
+//     name: "Brown Sugar Pearl Milk Tea",
+//     price: 19000,
+//   },
+//   {
+//     id: 7,
+//     image: null,
+//     name: "Pearl Milk Tea",
+//     price: 22000,
+//   },
+//   {
+//     id: 8,
+//     image: null,
+//     name: "Oats Milk Tea",
+//     price: 22000,
+//   },
+//   {
+//     id: 9,
+//     image: null,
+//     name: "Coconut Jelly Milk Tea",
+//     price: 22000,
+//   },
+//   {
+//     id: 10,
+//     image: null,
+//     name: "Red Bean Milk Tea",
+//     price: 22000,
+//   },
+//   {
+//     id: 11,
+//     image: null,
+//     name: "Fresh Squeezed Lemonade",
+//     price: 10000,
+//   },
+//   {
+//     id: 12,
+//     image: null,
+//     name: "Peach Earl Grey Tea",
+//     price: 16000,
+//   },
+//   {
+//     id: 13,
+//     image: null,
+//     name: "Original Jasmine Tea",
+//     price: 10000,
+//   },
+//   {
+//     id: 14,
+//     image: null,
+//     name: "Original Earl Grey Tea",
+//     price: 10000,
+//   },
+// ];
 
-  return results.length;
-}
-
-export function getListProducts(search, sortname, sortprice, page, limit) {
-  const results = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
-  );
-
-  if (sortname === "asc") {
-    results.sort((a, b) => a.name.localeCompare(b.name));
-  } else if (sortname === "desc") {
-    results.sort((a, b) => b.name.localeCompare(a.name));
+export async function getTotalDataProducts(search) {
+  try {
+    return await prisma.product.count({
+      where: {
+        name: {
+          contains: search,
+          mode: "insensitive",
+        },
+      },
+    });
+  } catch (err) {
+    console.error("Error get total product:", err);
+    throw err;
   }
+}
 
-  if (sortprice === "asc") {
-    results.sort((a, b) => a.price - b.price);
-  } else if (sortprice === "desc") {
-    results.sort((a, b) => b.price - a.price);
+export async function getListProducts(
+  search,
+  sortname,
+  sortprice,
+  page,
+  limit
+) {
+  try {
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    const orderBy = [];
+
+    if (sortname === "asc") orderBy.push({ name: "asc" });
+    if (sortname === "desc") orderBy.push({ name: "desc" });
+
+    if (sortprice === "asc") orderBy.push({ price: "asc" });
+    if (sortprice === "desc") orderBy.push({ price: "desc" });
+
+    const results = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: search,
+          mode: "insensitive",
+        },
+      },
+      orderBy,
+      skip,
+      take,
+    });
+
+    return results;
+  } catch (err) {
+    console.error("Error get list products:", err);
+    throw err;
   }
-
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  const paginatedResults = results.slice(startIndex, endIndex);
-
-  return paginatedResults;
 }
 
-export function getProductsById(id) {
-  return products.find((product) => product.id === id);
-}
+export async function getProductsById(id) {
+  try {
+    const result = await prisma.product.findUnique({
+      where: { id: id },
+    });
 
-export function addDataProduct(data) {
-  data = { id: products.length + 1, ...data };
-  products.push(data);
-}
-
-export function updateProductById(id, body) {
-  const index = products.findIndex((p) => p.id == id);
-
-  if (index === -1) {
-    return null;
+    return result;
+  } catch (err) {
+    console.error("Error get detail product:", err);
+    throw err;
   }
-
-  products[index] = { ...products[index], ...body };
-  return products[index];
 }
 
-export function deleteProductById(id) {
-  const data = products.find((product) => product.id === id);
-  if (!data) {
-    return false;
-  }
+export async function addDataProduct(data) {
+  try {
+    const result = await prisma.product.create({
+      data: {
+        name: data.name,
+        price: data.price,
+      },
+    });
 
-  products = products.filter((product) => product.id !== id);
-  return true;
+    return result;
+  } catch (err) {
+    console.error("Error while add data product:", err);
+    throw err;
+  }
+}
+
+export async function updateProductById(id, body) {
+  try {
+    const result = await prisma.product.update({
+      where: { id: Number(id) },
+      data: { ...body },
+    });
+
+    return result;
+  } catch (err) {
+    if (err.code === "P2025") {
+      return null;
+    }
+
+    console.error("Error while update product:", err);
+    throw err;
+  }
+}
+
+export async function deleteProductById(id) {
+  try {
+    await prisma.product.delete({
+      where: { id: Number(id) },
+    });
+
+    return true;
+  } catch (err) {
+    if (err.code === "P2025") {
+      return false; // ID tidak ditemukan
+    }
+
+    console.error("Error while delete product:", err);
+    throw err;
+  }
 }
